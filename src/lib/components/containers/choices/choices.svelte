7650694b42c733469dafc671072a63541176470f
RomainSav <romain.sv@icloud.com>
2022-10-30 00:44:34 +0200

    Introduce Choices component

---
+++ src/lib/components/containers/choices/choices.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let lowercase = true;
+  let uppercase = true;
+  let numbers = true;
+  let specials = true;
+
+  const isDisabled = [lowercase, uppercase, numbers, specials].filter((val) => val).length === 1;
+</script>
+
+<div>
+  
+</div>
