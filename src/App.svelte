00b81f3278424240c63e9a113e96186debc486da
RomainSav <romain.sv@icloud.com>
2022-10-30 01:35:43 +0200

    Add Particles component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte";
   import { generate } from "./lib/utils/generator";
   import { Generator } from "./lib/components/containers/generator";
+  import { Particles } from "./lib/components/particles";
   
   let password: string;
   onMount(() => loadPassword());
@@ -11,11 +12,14 @@
   }
 </script>
 
-<main class="min-h-screen bg-gradient-radial flex flex-col justify-center items-center">
-  <h1 class="text-neutral-1 font-medium text-4xl">Password Generator</h1>
+<main>
+  <Particles />
+  <div class="min-h-screen bg-gradient-radial flex flex-col justify-center items-center">
+    <h1 class="text-neutral-1 font-medium text-4xl">Password Generator</h1>
 
-  <div class="mt-10 w-full">
-    <Generator />
+    <div class="mt-10 w-full">
+      <Generator />
+    </div>
   </div>
 </main>
 
---
+++ src/lib/components/particles/particles.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Particles from "svelte-particles";
+  import { loadFull } from "tsparticles";
+  import config from "../../utils/particles.json";
+</script>
+
+<Particles id="tsparticles" options={config} particlesInit={(engine) => loadFull(engine)}/>
