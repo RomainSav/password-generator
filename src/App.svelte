1996dfa2bb6d7cc839affe084ba799573070965c
RomainSav <romain.sv@icloud.com>
2022-10-29 23:30:31 +0200

    Introduce Generator component

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   import { generate } from "./lib/utils/generator";
+  import { Generator } from "./lib/components/containers/generator";
 
   $: password = generate(16, true, true, true);
 </script>
 
 <main class="min-h-screen bg-gradient-radial flex justify-center items-center">
-  <h1 class="text-neutral-1 font-medium text-4xl">Password Generator</h1>
-
   <div>
-  
+    <h1 class="text-neutral-1 font-medium text-4xl">Password Generator</h1>
+
+    <div class="mt-10">
+      <Generator />
+    </div>
   </div>
 </main>
 
---
+++ src/lib/components/containers/generator/generator.svelte
@@ -0,0 +1,3 @@
+<form class="h-20 w-20 bg-gray-1 rounded-md shadow-md">
+  
+</form>
