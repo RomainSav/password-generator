b4f119ce3d2d5f7c2aeeea467370584647e46231
RomainSav <romain.sv@icloud.com>
2022-10-30 02:16:52 +0200

    Refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { generate } from "./lib/utils/generator";
   import { Generator } from "./lib/components/containers/generator";
   import { Particles } from "./lib/components/particles";
 </script>
@@ -8,7 +6,7 @@
 <main>
   <Particles />
   <div class="min-h-screen bg-gradient-radial flex flex-col justify-center items-center">
-    <h1 class="text-neutral-1 font-medium text-4xl">Password Generator</h1>
+    <h1 class="text-neutral-1 font-medium text-4xl relative z-20">Password Generator</h1>
 
     <div class="mt-10 w-full relative z-20">
       <Generator />
