32b7872594ed51c201cdcd371ae225389760f272
RomainSav <romain.sv@icloud.com>
2022-10-30 02:46:21 +0200

    Update choices.svelte

--- src/lib/components/containers/choices/choices.svelte
+++ src/lib/components/containers/choices/choices.svelte
@@ -10,8 +10,8 @@
 </script>
 
 <div class="grid grid-cols-2 gap-x-6">
-  <Checkbox disabled={isDisabled && lowercase} label="Upper case" bind:checked={lowercase}/>
-  <Checkbox disabled={isDisabled && uppercase} label="Lower case" bind:checked={uppercase}/>
+  <Checkbox disabled={isDisabled && lowercase} label="Lower case" bind:checked={lowercase}/>
+  <Checkbox disabled={isDisabled && uppercase} label="Upper case" bind:checked={uppercase}/>
   <Checkbox disabled={isDisabled && numbers} label="Numbers" bind:checked={numbers}/>
   <Checkbox disabled={isDisabled && specials} label="Symbols" bind:checked={specials}/>
 </div>
