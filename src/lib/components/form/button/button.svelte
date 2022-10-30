5f89b194ff781c0feac31c1f4167482a9532ec1f
RomainSav <romain.sv@icloud.com>
2022-10-30 02:12:27 +0200

    Update button.svelte

--- src/lib/components/form/button/button.svelte
+++ src/lib/components/form/button/button.svelte
@@ -3,6 +3,6 @@
   export let onClick: () => void = () => null;
 </script>
 
-<button on:click={onClick} class="bg-primary-1 rounded-md py-2 px-4 text-neutral-2 font-medium w-full hover:bg-primary-2 transition-colors duration-200">
+<button on:click={onClick} class="bg-primary-1 rounded-md py-2 px-4 text-neutral-1 font-medium w-full hover:bg-primary-2 transition-colors duration-200">
   <slot />
 </button>
